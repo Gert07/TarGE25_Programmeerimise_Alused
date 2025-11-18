1d3b00400080e1330c4afb94f26cf203c95264a5
Gert07 <puuramartin@gmail.com>
2025-11-18 14:10:52 +0200

    final push

--- matemaatilised_avaldised/ex01_matemaatilised_avaldised/solution.py
+++ matemaatilised_avaldised/ex01_matemaatilised_avaldised/solution.py
@@ -1,4 +1,8 @@
 """Math exercises."""
+from math import remainder
+import math
+
+from numpy.ma.core import multiply
 
 
 def sum_and_difference(num_a: int, num_b: int) -> tuple:
@@ -16,52 +20,72 @@ def float_division(num_a: int, num_b: int) -> float:
 
 def integer_division(num_a: int, num_b: int) -> int:
     """Divide given variables num_a and num_b and return the result rounded down."""
-    division = int(num_a / num_b)
+    division = num_a // num_b
     return division
 
 
 def powerful_operations(num_a: int, num_b: int) -> tuple:
     """Return the product of given variables, num_a to the power of num_b and the remainder of division of variables."""
     multiply_numbers = num_a * num_b
+    power = num_a ** num_b
+    remainder = num_a % num_b
     return multiply_numbers, power, remainder
 
 
 def find_average(num_a: int, num_b: int) -> float:
     """Return the average of given variables."""
-    # Write your code here
+    average = (num_a + num_b) / 2
     return average
 
 
 def area_of_a_circle(radius: float) -> float:
     """Calculate and return the area of a circle."""
-    # Write your code here
-    return circle_area
+    circle_area = math.pi ** radius
+    return round(circle_area, 2)
 
 
 def area_of_an_equilateral_triangle(side_length: float) -> int:
     """Calculate and return the area of an equilateral triangle."""
-    # Write your code here
-    return triangle_area
+    triangle_area = math.sqrt(3) / 4 * side_length ** 2
+    return round(triangle_area, 0)
 
 
 def calculate_discriminant(a: int, b: int, c: int) -> int:
     """Calculate discriminant with given variables and return the result."""
-    # Write your code here
+    discriminant = b**2 - 4*a*c
     return discriminant
 
 
 def calculate_hypotenuse_length(a: int, b: int) -> float:
     """Return the length of hypotenuse when the lengths of the catheti are given."""
-    # Write your code here
+    c = math.sqrt(a**2+b**2)
     return c
 
 
 def calculate_cathetus_length(a: int, c: int) -> float:
     """Return the length of cathetus when the lengths of the second cathetus and hypotenuse are given."""
-    # Write your code here
+    b = math.sqrt(c**2 - a**2)
     return b
 
 if __name__ == '__main__':
     addition_result, difference =  sum_and_difference(5, 6)
     assert addition_result == 11
     assert difference == -1
+
+    float_division_result = float_division (6,2)
+    assert 2.99 < float_division_result < 3.01
+
+    integer_division_result = integer_division(10,2)
+    assert isinstance(integer_division_result, int)
+    assert integer_division_result == 5
+
+    multiply_numbers, power, remainder = powerful_operations(4,5)
+    assert multiply_numbers == 20
+    assert power == 4 ** 5
+    assert remainder == 4
+
+    find_average_result = find_average(5, 9)
+    assert find_average_result == 7
+
+    circle_area_result = area_of_a_circle(5)
+    assert circle_area_result == round(math.pi ** 5, 2)
