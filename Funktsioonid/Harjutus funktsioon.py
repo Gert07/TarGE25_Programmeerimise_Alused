d1307d937279106d854e7d1058c747a215938d3a
Gert07 <puuramartin@gmail.com>
2025-11-29 08:55:32 +0200

    Parandused sisse viidud

--- Funktsioonid/Harjutus funktsioon.py
+++ Funktsioonid/Harjutus funktsioon.py
@@ -1,24 +1,32 @@
 """Function examples."""
 
 
-#func()
+# func()
 def func():
-    print("I'm inside the function")
+    """Print a message from inside the function."""
+    print('I´m inside the function')
 
-#my_name_is(name)
+
+# my_name_is(name)
 def my_name_is(name: str) -> str:
+    """Print given name."""
     print(f"My name is {name}")
 
-#sum_six(num)
-def sum_six(num: int) -> str:
-    return f"{num}+{6}={num+6}"
 
-#sum_numbers()
+# sum_six(num)
+def sum_six(num: int):
+    """Return the sum of 6 digits."""
+    return num + 6
+
+
+# sum_numbers()
 def sum_numbers(a, b: int):
-    return f"{a}+{b}={a+b}"
+    """Return the sum of (a, b)."""
+    return a + b
 
-#usd_to_eur()
+
+# usd_to_eur()
 def usd_to_eur(dollars: int):
+    """Convert dollars to Euro."""
     eur = (dollars * 0.8)
     return eur
-
